fc1d4f601c2cdc34509d1d7101878c24e01f0e34
Dan Bruder <dbruder@rapiddg.com>
2017-01-31 12:02:20 -0500

    semantic ui css

---
+++ app/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import api from './api'
+
+export default class extends React.Component{
+  componentDidMount(){
+  }
+  render() {
+    return (
+      <div className="container">
+        header
+      </div>
+    )
+  }
+
+}
