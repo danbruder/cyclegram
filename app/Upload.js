64939d417d1c3763ccb5ff0b14c82951f5a5f5fd
Dan Bruder <dbruder@rapiddg.com>
2017-02-02 09:31:14 -0500

    Created a post

--- app/Upload.js
+++ app/Upload.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 import { Container,  Button, Checkbox, Form  } from 'semantic-ui-react'
 import api from './api'
+
 const CLOUDINARY_UPLOAD_PRESET = 'ko9eaxls';
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dylrswykj/upload';
 
@@ -15,7 +16,16 @@ export default class extends Component {
 
   handleSubmit(e, { formData }){
     e.preventDefault()
-    console.log(formData)
+    const postService = api.service("posts")
+
+    postService.create({
+      imageSrc: this.state.uploadedFileCloudinaryUrl,
+      text: formData.description
+    })
+    .then(result => {
+      this.props.push("/")
+    })
+    .catch(err => console.error(err))
   }
 
   onImageDrop(files){
@@ -58,7 +68,7 @@ export default class extends Component {
           }
         </div>
         <Form onSubmit={this.handleSubmit.bind(this)}>
-          <Form.TextArea name='details' label='Details' placeholder='Anything else we should know?' rows='3' />
+          <Form.TextArea name='description' label='Details' placeholder='Anything else we should know?' rows='3' />
           <Button primary type='submit'>Submit</Button>
         </Form>
       </div>
--- src/services/post/post-model.js
+++ src/services/post/post-model.js
@@ -9,7 +9,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  text: { type: String, required: true },
+  imageSrc: { type: String, required: true },
+  description: { type: String, required: false },
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
