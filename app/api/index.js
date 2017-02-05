7042744f87e9d5df958e0d89bbf2008444747d08
Dan Bruder <dbruder@rapiddg.com>
2017-02-04 21:16:14 -0500

    foo

--- app/api/index.js
+++ app/api/index.js
@@ -4,7 +4,7 @@ import socketio from 'feathers-socketio/client';
 import io from 'socket.io-client';
 import authentication from 'feathers-authentication/client';
 
-const socket = io('localhost:3030');
+const socket = io('https://secure-anchorage-46467.herokuapp.com');
 
 export default feathers()
 .configure(hooks())
